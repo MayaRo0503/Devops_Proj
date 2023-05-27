ee0d04441e5c0b3ffdb0646ea244f2996d7a9143
mayarozen <maya.rozen53@gmail.com>
2023-05-27 12:49:51 +0300

    update 3

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const app = require("./server");
+const app = require("./app");
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 const port = process.env.PORT || 3000;
 
--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require("express");
-
 const app = express();
+const fs = require("fs");
+const path = require("path");
+
+// Define the path to the student grades file
+const filePath = path.join(__dirname, "data.txt");
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  fs.readFile("C:\3rdDevops_Projdata.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    const students = data.split("\n").map((line) => {
+      const [name, exam1, exam2, exam3] = line.split(",");
+      return { name, exam1, exam2, exam3 };
+    });
+
+    res.render("register", { students });
+  });
 });
-app.get("/test", (req, res) => {
-  res.send("test Hello");
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
 
 module.exports = app;
