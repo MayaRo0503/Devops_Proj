671223be365064b77929698cf13994a8e1737997
mayarozen <maya.rozen53@gmail.com>
2023-05-27 12:01:41 +0300

    1

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('Hello')
-})
-app.get('/test', (req,res)=>{
-    res.send('test Hello')
-})
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+app.get("/test", (req, res) => {
+  res.send("test Hello");
+});
 
-module.exports = app
+module.exports = app;
