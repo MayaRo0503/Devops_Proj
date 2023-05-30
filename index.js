2d7f970100c63ffb09a7331ab89f6f68fcbf4743
mayarozen <maya.rozen53@gmail.com>
2023-05-30 12:21:29 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const app = require("./server");
 const connect = require("./connect");
+const express = require("express");
+const router = express.Router();
+const Grade = require("../models/Grade");
 
 connect();
+
+router.post("/", async (req, res) => {
+  try {
+    // Extract data from the request body
+    const { fullName, grade1, grade2, grade3 } = req.body;
+
+    // Create a new Grade document
+    const grade = new Grade({
+      fullName,
+      grade1,
+      grade2,
+      grade3,
+    });
+
+    // Save the grade document to the database
+    const savedGrade = await grade.save();
+
+    res.status(201).send(savedGrade);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Error occurred while saving grades" });
+  }
+});
 
 const port = process.env.PORT || 3001;
 
